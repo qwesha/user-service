0d8d346cbb514631cf5c0cd77294e52c30518a41
geo <joraapple@icloud.com>
2025-02-22 13:51:59 +0300

    model.user

---
+++ user-service/src/main/java/ru/petproject/ecommerce/user_service/model/User.java
@@ -0,0 +1,19 @@
+package ru.petproject.ecommerce.user_service;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String email;
+    private String password;
+    private String name;
+    private String role; // Роли: USER, ADMIN
+    private LocalDateTime createdAt;
+}
